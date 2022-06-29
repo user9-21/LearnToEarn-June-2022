8a5747871655bad53e50d1a6a0c119c1d3c80fed
user9-21 <92918900+user9-21@users.noreply.github.com>
2022-06-29 20:20:53 +0530

    initial

---
+++ files/sample.sh
@@ -0,0 +1,23 @@
+PROJECT_ID=`gcloud projects list  --format='value(project_id)'| grep qwiklabs-gcp | head -1`
+gcloud config set project $PROJECT_ID
+
+curl -o default.sh https://raw.githubusercontent.com/user9-21/LearnToEarn-June-2022/main/files/default.sh
+source default.sh
+
+echo "${BOLD}${CYAN}$PROJECT_ID ${RESET}"
+
+
+completed "Task 1"
+
+
+completed "Task 2"
+
+
+completed "Task 3"
+
+
+completed "Task 4"
+
+completed "Lab"
+
+remove_files
